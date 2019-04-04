17b7380a1619b06d3e3f2b2ff9d8df2e56c86f9d
Denis Kovalchuck <deniskovalchuck@yandex,ua>
2019-04-04 15:07:53 +0300

    Исправлены ошибки, добавлен параметр в тип предмета

--- University/Logics/Books/TypeSubject.cs
+++ University/Logics/Books/TypeSubject.cs
@@ -8,10 +8,14 @@ namespace Logics.Books
     {
         public struct StructTypeSubject
         {
+            public type_lesson typelesson;
             public string name;
             public int id;
         }
-
+        public enum type_lesson {
+            Session ='S',
+            Study = 'O'
+        }
         #region Variable
         public string exception = "";
         private  Functions.Connection.ConnectionDB _connectionDB =null;
@@ -30,7 +34,15 @@ namespace Logics.Books
                 using (var reader = cmd.ExecuteReader())
                     while (reader.Read())
                     {
-                        disciplines.Add(new StructTypeSubject() { id = reader.GetInt32(0), name = reader.GetString(1)});
+                        switch (reader.GetChar(2))
+                        {
+                            case 'S':
+                                disciplines.Add(new StructTypeSubject() { id = reader.GetInt32(0), name = reader.GetString(1), typelesson = type_lesson.Session });
+                            break;
+                            case 'O':
+                                disciplines.Add(new StructTypeSubject() { id = reader.GetInt32(0), name = reader.GetString(1), typelesson = type_lesson.Study });
+                                break;
+                        }
                     }
                 conn.Close();
                 return true;
@@ -41,15 +53,25 @@ namespace Logics.Books
                 return false;
             }
         }
-        public bool AddTypeSubject(string name)
+        public bool AddTypeSubject(string name,type_lesson type_Lesson )
         {
             if (_connectionDB == null) { exception = "Подключение не установленно"; return false; }
             try
             {
                 var conn = new NpgsqlConnection(this._connectionDB.ConnectString);
                 conn.Open();
+                string com = "";
+                switch (type_Lesson)
+                {
+                    case type_lesson.Session:
+                        com = $"SELECT * from type_subject_add('{name}','S');";
+                        break;
+                    case type_lesson.Study:
+                        com = $"SELECT * from type_subject_add('{name}','O');";
 
-                using (var cmd = new NpgsqlCommand($"SELECT * from type_subject_add('{name}');", conn))
+                        break;
+                }
+                using (var cmd = new NpgsqlCommand(com, conn))
                 using (var reader = cmd.ExecuteReader())
                     if (reader.Read())
                     {
