743947a961026aa7609a1509c30ab89ddacf1cd4
SemikAlexander <36263835+SemikAlexander@users.noreply.github.com>
2019-04-29 21:41:27 +0300

    Форма генерации отчётов + отдельный поток

--- University/UniversityMain/GenerationDoc.Designer.cs
+++ University/UniversityMain/GenerationDoc.Designer.cs
@@ -29,18 +29,15 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.label4 = new System.Windows.Forms.Label();
             this.panel2 = new System.Windows.Forms.Panel();
-            this.TeacherBox = new System.Windows.Forms.ComboBox();
-            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.DepartmentBox = new System.Windows.Forms.ComboBox();
             this.label8 = new System.Windows.Forms.Label();
             this.FacultyBox = new System.Windows.Forms.ComboBox();
             this.label7 = new System.Windows.Forms.Label();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
-            this.MonthBox = new System.Windows.Forms.ComboBox();
             this.label2 = new System.Windows.Forms.Label();
             this.TypeDoc = new System.Windows.Forms.ComboBox();
             this.label3 = new System.Windows.Forms.Label();
@@ -48,9 +45,12 @@
             this.button3 = new System.Windows.Forms.Button();
             this.FolderPath = new System.Windows.Forms.TextBox();
             this.button4 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
             this.panel1.SuspendLayout();
-            this.panel2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
@@ -62,9 +62,19 @@
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(536, 44);
+            this.panel1.Size = new System.Drawing.Size(546, 44);
             this.panel1.TabIndex = 3;
             this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Panel1_MouseDown);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::UniversityMain.Properties.Resources.CreateDoc_50_;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(70, 44);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 15;
+            this.pictureBox1.TabStop = false;
             // 
             // label4
             // 
@@ -82,80 +92,10 @@
             this.panel2.Controls.Add(this.button2);
             this.panel2.Controls.Add(this.button1);
             this.panel2.Dock = System.Windows.Forms.DockStyle.Right;
-            this.panel2.Location = new System.Drawing.Point(463, 0);
+            this.panel2.Location = new System.Drawing.Point(473, 0);
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(73, 44);
             this.panel2.TabIndex = 1;
-            // 
-            // TeacherBox
-            // 
-            this.TeacherBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.TeacherBox.FormattingEnabled = true;
-            this.TeacherBox.Location = new System.Drawing.Point(135, 119);
-            this.TeacherBox.Name = "TeacherBox";
-            this.TeacherBox.Size = new System.Drawing.Size(389, 23);
-            this.TeacherBox.TabIndex = 34;
-            this.TeacherBox.SelectedIndexChanged += new System.EventHandler(this.TeacherBox_SelectedIndexChanged);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(12, 120);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(117, 18);
-            this.label1.TabIndex = 33;
-            this.label1.Text = "Преподаватель";
-            // 
-            // DepartmentBox
-            // 
-            this.DepartmentBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.DepartmentBox.FormattingEnabled = true;
-            this.DepartmentBox.Location = new System.Drawing.Point(135, 90);
-            this.DepartmentBox.Name = "DepartmentBox";
-            this.DepartmentBox.Size = new System.Drawing.Size(389, 23);
-            this.DepartmentBox.TabIndex = 32;
-            this.DepartmentBox.SelectedIndexChanged += new System.EventHandler(this.DepartmentBox_SelectedIndexChanged);
-            // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label8.Location = new System.Drawing.Point(57, 91);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(72, 18);
-            this.label8.TabIndex = 31;
-            this.label8.Text = "Кафедра";
-            // 
-            // FacultyBox
-            // 
-            this.FacultyBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.FacultyBox.FormattingEnabled = true;
-            this.FacultyBox.Location = new System.Drawing.Point(135, 59);
-            this.FacultyBox.Name = "FacultyBox";
-            this.FacultyBox.Size = new System.Drawing.Size(389, 23);
-            this.FacultyBox.TabIndex = 30;
-            this.FacultyBox.SelectedIndexChanged += new System.EventHandler(this.FacultyBox_SelectedIndexChanged);
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label7.Location = new System.Drawing.Point(46, 60);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(83, 18);
-            this.label7.TabIndex = 29;
-            this.label7.Text = "Факультет";
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Image = global::UniversityMain.Properties.Resources.CreateDoc_50_;
-            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(70, 44);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox1.TabIndex = 15;
-            this.pictureBox1.TabStop = false;
             // 
             // button2
             // 
@@ -181,33 +121,51 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.Button1_Click);
             // 
-            // MonthBox
+            // DepartmentBox
             // 
-            this.MonthBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.MonthBox.FormattingEnabled = true;
-            this.MonthBox.Items.AddRange(new object[] {
-            "Январь",
-            "Февраль",
-            "Март",
-            "Апрель",
-            "Май",
-            "Июнь ",
-            "Июль",
-            "Август",
-            "Сентябрь",
-            "Октябрь",
-            "Ноябрь",
-            "Декабрь"});
-            this.MonthBox.Location = new System.Drawing.Point(135, 148);
-            this.MonthBox.Name = "MonthBox";
-            this.MonthBox.Size = new System.Drawing.Size(389, 23);
-            this.MonthBox.TabIndex = 36;
+            this.DepartmentBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.DepartmentBox.FormattingEnabled = true;
+            this.DepartmentBox.Location = new System.Drawing.Point(145, 90);
+            this.DepartmentBox.Name = "DepartmentBox";
+            this.DepartmentBox.Size = new System.Drawing.Size(389, 23);
+            this.DepartmentBox.TabIndex = 32;
+            this.DepartmentBox.SelectedIndexChanged += new System.EventHandler(this.DepartmentBox_SelectedIndexChanged);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label8.Location = new System.Drawing.Point(67, 91);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(72, 18);
+            this.label8.TabIndex = 31;
+            this.label8.Text = "Кафедра";
+            // 
+            // FacultyBox
+            // 
+            this.FacultyBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FacultyBox.FormattingEnabled = true;
+            this.FacultyBox.Location = new System.Drawing.Point(145, 59);
+            this.FacultyBox.Name = "FacultyBox";
+            this.FacultyBox.Size = new System.Drawing.Size(389, 23);
+            this.FacultyBox.TabIndex = 30;
+            this.FacultyBox.SelectedIndexChanged += new System.EventHandler(this.FacultyBox_SelectedIndexChanged);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.Location = new System.Drawing.Point(56, 60);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(83, 18);
+            this.label7.TabIndex = 29;
+            this.label7.Text = "Факультет";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label2.Location = new System.Drawing.Point(76, 149);
+            this.label2.Location = new System.Drawing.Point(86, 120);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(53, 18);
             this.label2.TabIndex = 35;
@@ -221,16 +179,17 @@
             "Ставка",
             "Полставки",
             "Почасовка"});
-            this.TypeDoc.Location = new System.Drawing.Point(135, 177);
+            this.TypeDoc.Location = new System.Drawing.Point(145, 148);
             this.TypeDoc.Name = "TypeDoc";
             this.TypeDoc.Size = new System.Drawing.Size(389, 23);
             this.TypeDoc.TabIndex = 38;
+            this.TypeDoc.SelectedIndexChanged += new System.EventHandler(this.TypeDoc_SelectedIndexChanged);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label3.Location = new System.Drawing.Point(45, 178);
+            this.label3.Location = new System.Drawing.Point(55, 149);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(84, 18);
             this.label3.TabIndex = 37;
@@ -240,7 +199,7 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label5.Location = new System.Drawing.Point(47, 207);
+            this.label5.Location = new System.Drawing.Point(57, 204);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(82, 18);
             this.label5.TabIndex = 39;
@@ -250,7 +209,7 @@
             // 
             this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
-            this.button3.Location = new System.Drawing.Point(430, 237);
+            this.button3.Location = new System.Drawing.Point(440, 231);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(94, 30);
             this.button3.TabIndex = 40;
@@ -262,7 +221,7 @@
             // 
             this.FolderPath.Enabled = false;
             this.FolderPath.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
-            this.FolderPath.Location = new System.Drawing.Point(135, 206);
+            this.FolderPath.Location = new System.Drawing.Point(145, 204);
             this.FolderPath.Name = "FolderPath";
             this.FolderPath.Size = new System.Drawing.Size(389, 21);
             this.FolderPath.TabIndex = 41;
@@ -273,28 +232,58 @@
             this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
             this.button4.Image = global::UniversityMain.Properties.Resources.OK;
-            this.button4.Location = new System.Drawing.Point(0, 273);
+            this.button4.Location = new System.Drawing.Point(0, 270);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(536, 47);
+            this.button4.Size = new System.Drawing.Size(546, 37);
             this.button4.TabIndex = 42;
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.Button4_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(12, 179);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(131, 18);
+            this.label6.TabIndex = 43;
+            this.label6.Text = "Начало семестра";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.CustomFormat = "";
+            this.dateTimePicker1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker1.Location = new System.Drawing.Point(145, 177);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(389, 21);
+            this.dateTimePicker1.TabIndex = 44;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.CustomFormat = "MMMM yyyy";
+            this.dateTimePicker2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.dateTimePicker2.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker2.Location = new System.Drawing.Point(145, 119);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(389, 21);
+            this.dateTimePicker2.TabIndex = 45;
             // 
             // GenerationDoc
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(536, 320);
+            this.ClientSize = new System.Drawing.Size(546, 307);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label6);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.FolderPath);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.TypeDoc);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.MonthBox);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.TeacherBox);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.DepartmentBox);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.FacultyBox);
@@ -307,8 +296,8 @@
             this.Load += new System.EventHandler(this.GenerationDoc_Load);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
-            this.panel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel2.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -322,13 +311,10 @@
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.ComboBox TeacherBox;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ComboBox DepartmentBox;
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.ComboBox FacultyBox;
         private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.ComboBox MonthBox;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.ComboBox TypeDoc;
         private System.Windows.Forms.Label label3;
@@ -336,5 +322,8 @@
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.TextBox FolderPath;
         private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
     }
 }
